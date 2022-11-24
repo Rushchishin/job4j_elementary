73aca07e05a3b67220ec254a21abb508107caf2f
Rushchishin <Rushishin@rambler.ru>
2022-11-24 22:41:53 +0300

    Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Petr Rush");
+        System.out.println("24.11.2022");
+    }
+}
